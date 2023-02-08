8642bec8bae9f419588c1d423f60defa67700a02
Hash Milhan <hashirm@gmail.com>
2023-02-08 14:15:42 +0000

    updates to send just one email

--- app.py
+++ app.py
@@ -1,48 +1,52 @@
 import smtplib
 import pandas as pd
 from email.mime.text import MIMEText
-import configparser
+from email.mime.multipart import MIMEMultipart
 from datetime import datetime, timedelta
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+address = config["email"]["address"]
+password = config["email"]["password"]
+recipient = config["recipient"]["address"]
 
 
-def send_email(product, expiry_date):
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-
-    address = config["email"]["address"]
-    password = config["email"]["password"]
-    to_address = config["recipient"]["address"]
-
-    server = smtplib.SMTP("smtp.gmail.com", 587)
+def send_email(product_list):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
+    msg = MIMEMultipart()
+    msg['From'] = address
+    msg['To'] = recipient
+    msg['Subject'] = "Subscriptions expiring soon"
+    body = "The following subscriptions are expiring within the next 7 days: \n\n"
+    for product in product_list:
+        body += f"{product}\n"
+    msg.attach(MIMEText(body, 'plain'))
+
     server.login(address, password)
-
-    subject = f"Subscription Reminder: {product}"
-    body = f"Your {product} subscription will expire on {expiry_date}.\nPlease check if you need to renew it or cancel it."
-
-    msg = MIMEText(body)
-    msg["Subject"] = subject
-    msg["From"] = address
-    msg["To"] = to_address
-
-    server.send_message(msg)
-    print("Email sent successfully!")
+    text = msg.as_string()
+    server.sendmail(address, recipient, text)
+    print("Email sent!")
     server.quit()
 
 
 def main():
-    df = pd.read_excel("subscriptions.xlsx")
     today = datetime.now().date()
+    df = pd.read_excel("subscriptions.xlsx")
+    product_list = []
     for index, row in df.iterrows():
-        product = row["Product"]
-        expiry_date = row["Expiry Date"]
-        expiry_date = expiry_date.date()
-        days_until_expiry = (expiry_date - today).days
-        if days_until_expiry == 7:
-            send_email(product, expiry_date)
+        expiry_date = row['Expiry Date'].date()
+        if (expiry_date - today).days <= 7:
+            product_list.append(row['Product'])
+    if product_list:
+        send_email(product_list)
+    else:
+        print("No subscriptions expiring within the next 7 days.")
 
 
 if __name__ == "__main__":
